5667f8d6a7d94de7056745c945774c6f1535c4b5
Hoorain Sundus <hoorain.sunduss@gmail.com>
2025-08-16 17:05:32 +0500

    If else

--- Practise.js
+++ Practise.js
@@ -1,1 +1,13 @@
 console.log('Hello')
+
+//---------------- If else -------------------
+
+let age = 15;
+let status = "None";
+if(age >= 18){
+    status = "Adult";
+    console.log(status)
+} else{
+    status = "Young";
+    console.log(status)
+}
