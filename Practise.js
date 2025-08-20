65a3d0a5e2e160411ca6b70830f238515e194e21
Hoorain Sundus <hoorain.sunduss@gmail.com>
2025-08-20 10:21:54 +0500

    Switch

--- Practise.js
+++ Practise.js
@@ -1,13 +1,42 @@
-console.log('Hello')
+// console.log('Hello')
 
 //---------------- If else -------------------
 
-let age = 15;
-let status = "None";
-if(age >= 18){
-    status = "Adult";
-    console.log(status)
-} else{
-    status = "Young";
-    console.log(status)
+// let age = 15;
+// let status = "None";
+// if(age >= 18){
+//     status = "Adult";
+//     console.log(status)
+// } else{
+//     status = "Young";
+//     console.log(status)
+// }
+
+
+//---------- Switch ---------------------------
+let day = 3;
+let dayName;
+
+switch (day) {
+    case 1:
+        dayName = "Sunday";
+        break;
+    case 2:
+        dayName = "Monday";
+        break;
+    case 3:
+        dayName = "Tuesday";
+        break;
+    case 4:
+        dayName = "Wednesday";
+    case 5:
+        dayName = "Thursday";
+    case 6:
+        dayName = "Fridya";
+    case 7:
+        dayName = "Saturday"
+    default:
+        dayName = "Invalid day";
 }
+
+console.log(dayName); //Output Tuesday
