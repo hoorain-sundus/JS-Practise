093dfce5313db379cf8d853148b61f25622b9aa4
Hoorain Sundus <hoorain.sunduss@gmail.com>
2025-08-24 14:54:42 +0500

    Array Method

--- Practise.js
+++ Practise.js
@@ -48,5 +48,7 @@
 // myArray.push("strawberry");
 // console.log(myArray);
 
-
-
+// let myArray = [1, 2, 3, 4, 5];
+// let lastElement = myArray.pop();
+// console.log(myArray);
+// console.log(lastElement);
